65411b945f24f4a2217b631f25a2d44dfce4ca9a
Harrison Chapman <hchap1@hotmail.com>
2026-06-01 23:39:08 +1000

    fix: strip comments to parse waybar jsonc

--- home/packages/waybar.nix
+++ home/packages/waybar.nix
@@ -3,7 +3,12 @@
 {
   programs.waybar = {
     enable = true;
-    settings = builtins.fromJSON (builtins.readFile ../config/waybar/config.jsonc);
+	settings = builtins.fromJSON (builtins.readFile (
+	  pkgs.runCommand "strip-comments" {} ''
+		${pkgs.nodePackages.strip-json-comments-cli}/bin/strip-json-comments \
+		  ${../config/waybar/config.jsonc} > $out
+	  ''
+	));
 	style    = builtins.readFile ../config/waybar/style.css;
   };
 
