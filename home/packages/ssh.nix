ae87add3967ae34681ab6a07d03997877f88e20b
Harrison Chapman <hchap1@hotmail.com>
2026-06-03 00:51:16 +1000

    feat: SSH remade

--- home/packages/ssh.nix
+++ home/packages/ssh.nix
@@ -1,14 +1,37 @@
-# Load ssh from ~/.ssh/id_ed25519
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  githubKey = "${config.home.homeDirectory}/.ssh/id_ed25519_github";
+in
 {
-	programs.ssh = {
-		enable = true;
+  programs.ssh = {
+    enable = true;
 
-		matchBlocks."github.com" = {
-			hostname = "github.com";
-			user = "git";
-			identityFile = "~/.ssh/id_ed25519";
-		};
-	};
+    # Ensures ssh-agent is used (optional but recommended)
+    addKeysToAgent = "yes";
+
+    matchBlocks = {
+      "github.com" = {
+        user = "git";
+        identityFile = githubKey;
+        identitiesOnly = true;
+      };
+    };
+
+    extraConfig = ''
+      Host *
+        ServerAliveInterval 60
+        ServerAliveCountMax 3
+    '';
+  };
+
+  home.activation.createGitHubSSHKey = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    if [ ! -f "${githubKey}" ]; then
+      echo "Generating GitHub SSH key..."
+      ${pkgs.openssh}/bin/ssh-keygen -t ed25519 \
+        -f "${githubKey}" \
+        -N "" \
+        -C "github-${config.home.username}@${config.networking.hostName or "nixos"}"
+    fi
+  '';
 }
