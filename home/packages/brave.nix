dae7554c408c33ec7f600f2e884ccdc14eeffba7
Harrison Chapman <hchap1@hotmail.com>
2026-06-02 14:37:30 +1000

    feat: brave ozone layer flag

--- home/packages/brave.nix
+++ home/packages/brave.nix
@@ -4,4 +4,17 @@
 	home.packages = with pkgs; [
 		brave
 	];
+
+	xdg.desktopEntries.brave = {
+		name = "Brave Web Browser";
+		exec = "brave --ozone-platform=wayland %U";
+		icon = "brave-browser";
+		categories = [ "Network" "WebBrowser" ];
+		mimeType = [
+		  "text/html"
+		  "x-scheme-handler/http"
+		  "x-scheme-handler/https"
+		];
+		terminal = false;
+	};
 }
