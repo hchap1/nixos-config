ca4213bbe431f2a8603fc7dc5b0887d1c799d4c8
Harrison Chapman <hchap1@hotmail.com>
2026-06-02 13:55:12 +1000

    feat: niri-level mako shadow

--- home/packages/mako.nix
+++ home/packages/mako.nix
@@ -6,7 +6,7 @@
 
 	  settings = {
 		font = "JetBrainsMono Nerd Font";
-		background-color = "#1e1e2e";
+		background-color = "#1e1e2e77";
 		text-color = "#cdd6f4";
 		border-color = "#cba6f7";
 		border-radius = 8;
@@ -17,6 +17,8 @@
 		anchor = "top-right";
 		shadow = true;
 		shadow-color = "#00000080";
+		padding=12;
+		outer-margin=10;
 	  };
 	};
 }
