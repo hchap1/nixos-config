6503e3ea9450ba4b3e07ce212493af992bbee69e
Harrison Chapman <hchap1@hotmail.com>
2026-06-02 15:07:41 +1000

    feat: includ wlr portal

--- modules/portal.nix
+++ modules/portal.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 
 {
-	xdg.portal = {
-	  enable = true;
-	  extraPortals = with pkgs; [
-		xdg-desktop-portal-gtk
-	  ];
-	  config.common = {
-		default = [ "gtk" ];
-		"org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
-	  };
-	};
+  xdg.portal = {
+    enable = true;
+
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-wlr
+    ];
+
+    config.common.default = "gtk";
+  };
 }
