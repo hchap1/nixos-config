370cb41ec50c0f8da030d43188ecdf6c37dc2adf
Harrison Chapman <hchap1@hotmail.com>
2026-06-02 13:30:36 +1000

    feat: emoji font for waybar

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -3,6 +3,19 @@
 {
 	fonts.packages = with pkgs; [
 		nerd-fonts.jetbrains-mono
+		noto-fonts
+		noto-fonts-cjk-sans
 		noto-fonts-emoji
 	];
+
+	fonts.fontconfig = {
+		enable = true;
+
+		defaultFonts = {
+			monospace = [ "JetBrainsMono Nerd Font" ];
+			sansSerif = [ "Noto Sans" ];
+			serif = [ "Noto Serif" ];
+			emoji = [ "Noto Color Emoji" ];
+		};
+	};
 }
