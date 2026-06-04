1740e267d5198f34165158e257a39ad646ae32a9
Harrison Chapman <hchap1@hotmail.com>
2026-06-04 20:06:42 +1000

    fix: neovide syntax. I really need to read before pasting ...

--- home/packages/neovide.nix
+++ home/packages/neovide.nix
@@ -5,21 +5,17 @@
 		enable = true;
 	};
 
-	{ pkgs, ... }:
+	home.packages = [
+		(pkgs.writeShellScriptBin "edit" ''
+		 set -e
+		 target="''${1:-.}"
+		 target="$(realpath "$target")"
 
-	{
-		home.packages = [
-			(pkgs.writeShellScriptBin "edit" ''
-			 set -e
-			 target="''${1:-.}"
-			 target="$(realpath "$target")"
-
-			 if [ -d "$target" ]; then
-			 exec ${pkgs.neovide}/bin/neovide --cwd "$target"
-			 else
-			 exec ${pkgs.neovide}/bin/neovide "$target"
-			 fi
-			 '')
-		];
-	}
+		 if [ -d "$target" ]; then
+		 exec ${pkgs.neovide}/bin/neovide --cwd "$target"
+		 else
+		 exec ${pkgs.neovide}/bin/neovide "$target"
+		 fi
+		 '')
+	];
 }
