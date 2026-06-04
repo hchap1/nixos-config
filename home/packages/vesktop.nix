f4e7f1e7a98b0af11c63fa828784b5ba94f9ca87
Harrison Chapman <hchap1@hotmail.com>
2026-06-04 22:18:27 +1000

    feat: declaritive vesktop theme [hopefully]

--- home/packages/vesktop.nix
+++ home/packages/vesktop.nix
@@ -4,4 +4,24 @@
 	home.packages = with pkgs; [
 		vesktop
 	];
+
+	home.file.".config/vesktop/settings.json".text = builtins.toJSON {
+	  appBadge = false;
+	  arRPC = true;
+	  checkUpdates = false;
+
+	  customTitleBar = true;
+	  minimizeToTray = true;
+	  tray = true;
+
+	  splashTheming = true;
+	  splashBackground = "#1e1e2e";
+	  splashColor = "#cdd6f4";
+
+	  hardwareAcceleration = true;
+	  discordBranch = "stable";
+	};
+
+	home.file.".config/vesktop/settings/quickCss.css".source = ../../data/vesktop/theme.css;
+	home.file.".config/vesktop/themes/".source = ../../data/vesktop/theme.css;
 }
