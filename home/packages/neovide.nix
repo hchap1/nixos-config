14b2587cb4b1e9e6f16b0e455f46a20c4262e1a8
Harrison Chapman <hchap1@hotmail.com>
2026-06-04 20:27:15 +1000

    feat: better edit script

--- home/packages/neovide.nix
+++ home/packages/neovide.nix
@@ -8,15 +8,9 @@
 	home.packages = [
 		(pkgs.writeShellScriptBin "edit" ''
 		 set -e
-		 target="''${1:-.}"
+		 target="${1:-.}"
 		 target="$(realpath "$target")"
-
-		 if [ -d "$target" ]; then
-		 niri msg action spawn -- exec ${pkgs.neovide}/bin/neovide --cwd "$target"
-		 else
-		 niri msg action spawn exec ${pkgs.neovide}/bin/neovide "$target"
-		 fi
-		 exit
+		 niri msg action spawn -- "neovide" "$target"
 		 '')
 	];
 }
