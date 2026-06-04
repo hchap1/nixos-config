4ab6a87c44f2bd06cb6e67b5dcbcc922a9e5ab15
Harrison Chapman <hchap1@hotmail.com>
2026-06-04 17:33:01 +1000

    feat: noctalia theme colours

--- home/packages/noctalia.nix
+++ home/packages/noctalia.nix
@@ -2,8 +2,7 @@
 
 {
     programs.noctalia = {
-		enable = true;
-
+		enable = true
 		colors = {
 			mError = "#f38ba8";
 			mOnError = "#1e1e2e";
@@ -22,60 +21,5 @@
 			mSurfaceVariant = "#313244";
 			mTertiary = "#f5c2e7";
 		};
-
-      settings = {
-        # configure noctalia here
-        bar = {
-          density = "compact";
-          position = "right";
-          showCapsule = false;
-          widgets = {
-            left = [
-              {
-                id = "ControlCenter";
-                useDistroLogo = true;
-              }
-              {
-                id = "Network";
-              }
-              {
-                id = "Bluetooth";
-              }
-            ];
-            center = [
-              {
-                hideUnoccupied = false;
-                id = "Workspace";
-                labelMode = "none";
-              }
-            ];
-            right = [
-              {
-                alwaysShowPercentage = false;
-                id = "Battery";
-                warningThreshold = 30;
-              }
-              {
-                formatHorizontal = "HH:mm";
-                formatVertical = "HH mm";
-                id = "Clock";
-                useMonospacedFont = true;
-                usePrimaryColor = true;
-              }
-            ];
-          };
-        };
-        colorSchemes.predefinedScheme = "Monochrome";
-        # general = {
-          # avatarImage = "/home/drfoobar/.face";
-          # radiusRatio = 0.2;
-        # };
-        location = {
-          monthBeforeDay = false;
-          name = "Brisbane, Australia";
-        };
-      };
-      # this may also be a string or a path to a JSON file.
-    };
- 
+	 }
 }
