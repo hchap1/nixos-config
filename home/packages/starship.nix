5940fe8e1274d891578d00a4b897d7faaf30a897
Harrison Chapman <hchap1@hotmail.com>
2026-06-04 21:46:09 +1000

    feat: starship

--- home/packages/starship.nix
+++ home/packages/starship.nix
@@ -1,18 +1,72 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-	home.packages = with pkgs; [
-		starship
-	];
+  programs.starship = {
+    enable = true;
 
-	programs.starship = {
-		enable = true;
+    settings = {
+      add_newline = true;
 
-		enableFishIntegration = true;
+      # Line 1: full path + git branch
+      format = ''
+        $directory$git_branch
+        $character
+      '';
 
-		settings = {
-			add_newline = true;
-			format = "$all$directory$character";
-		};
-	};
+      palette = "catppuccin_mocha";
+
+      # Full path directory
+      directory = {
+        truncation_length = 0;
+        truncate_to_repo = false;
+
+        style = "mauve bold";
+      };
+
+      git_branch = {
+        format = "[$branch]($style)";
+        style = "lavender";
+      };
+
+      character = {
+        success_symbol = "[❯](mauve bold) ";
+        error_symbol = "[❯](red bold) ";
+        vicmd_symbol = "[❮](mauve bold) ";
+      };
+
+      # Catppuccin Mocha palette (mauve-forward tweak)
+      palettes.catppuccin_mocha = {
+        rosewater = "#f5e0dc";
+        flamingo  = "#f2cdcd";
+        pink      = "#f5c2e7";
+        mauve     = "#cba6f7";
+        red       = "#f38ba8";
+        maroon    = "#eba0ac";
+        peach     = "#fab387";
+        yellow    = "#f9e2af";
+        green     = "#a6e3a1";
+        teal      = "#94e2d5";
+        sky       = "#89dceb";
+        sapphire  = "#74c7ec";
+        blue      = "#89b4fa";
+        lavender  = "#b4befe";
+
+        text      = "#cdd6f4";
+        subtext1  = "#bac2de";
+        subtext0  = "#a6adc8";
+
+        overlay2  = "#9399b2";
+        overlay1  = "#7f849c";
+        overlay0  = "#6c7086";
+
+        surface2  = "#585b70";
+        surface1  = "#45475a";
+        surface0  = "#313244";
+
+        base      = "#1e1e2e";
+        mantle    = "#181825";
+        crust     = "#11111b";
+      };
+    };
+  };
 }
